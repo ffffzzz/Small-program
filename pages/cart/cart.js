9425f79a8bbfd1dc9cc786e83ae957ef54b1405b
ffaaaaa <239157719@qq.com>
2020-04-12 11:22:54 +0800

    “首二次提交”

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -5,9 +5,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    Receiving: {},
+    cart: []
   },
-
+  // 点击按钮获取收货地址
+  btnclick() {
+    // 1.1获取权限状态
+    wx.getSetting({
+      success(result) {
+        // 1.2获取权限状态 主要发现一些属性名很怪异的时候 都要用[]来获取属性值
+        const Address = result.authSetting["scopre.address"]
+        if (Address === true || Address === undefined) {
+          wx.chooseAddress({
+            success(res1) {
+              const Receiving = res1
+                  // 存入缓存中
+                  wx.setStorageSync('Receiving', Receiving)
+            }
+          })
+        }else{
+          // 1.3 用户以前拒绝过授予权限 诱导用户再次获取权限
+          wx.openSetting({
+            success() {
+              wx.chooseAddress({
+              })
+            }
+          })
+        }
+      },
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -26,7 +53,13 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    // 1获取缓存中收货地址信息
+    const Receiving = wx.getStorageSync('Receiving');
+    // 2获取缓存中的购物车数据
+    const cart = wx.getStorageSync('cart')
+    this.setData({
+      Receiving,cart
+    })
   },
 
   /**
--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -6,26 +6,27 @@ Page({
    * 页面的初始数据
    */
   data: {
+    Goods: {},
     iid: '',
+    // num: 1,
     tabControl: ["商品","参数","评论","推荐"],
-    topImg: [],
-    title: '',
-    newprice: '',
-    oldprice: '',
-    columns: [],
-    services: [],
-    name: '',
-    shoplogo: '',
-    cSells: '',
-    cGoods: "",
-    score: [],
-    desc: '',
-    detaiimg: [],
+    // topImg: [],
+    // title: '',
+    // newprice: '',
+    // oldprice: '',
+    // columns: [],
+    // services: [],
+    // name: '',
+    // shoplogo: '',
+    // cSells: '',
+    // cGoods: "",
+    // score: [],
+    // desc: '',
+    // detaiimg: [],
     isBackTop: false
   },
-  itemclick() {
-
-  },
+  // 全局对象
+  GoodsInfo: {},
   /**
    * 生命周期函数--监听页面加载
    */
@@ -44,25 +45,28 @@ Page({
     var iid = this.data.iid
     getdetailUrl(iid).then(res => {
       // console.log(res)
+      const Goods = res;
+      this.GoodsInfo = Goods;
       // 1.2取出轮播图并设置到data中
-      const data = res.data.result;
-      const topImg = data.itemInfo.topImages;
+      // const data = res.data.result;
+      // const topImg = data.itemInfo.topImages;
       // 1.3取出商品描述
-      const title = data.itemInfo.title;
-      const newprice = data.itemInfo.lowNowPrice;
-      const oldprice = data.itemInfo.oldPrice;
-      const columns = data.columns;
-      const name = data.shopInfo.name;
-      const shopLogo = data.shopInfo.shopLogo;
-      const services = data.shopInfo.services;
-      const cSells = data.shopInfo.cSells;
-      const cGoods = data.shopInfo.cGoods;
-      const score = data.shopInfo.score;
-      const desc = data.detailInfo.desc;
-      const detaiimg = data.detailInfo.detailImage
+      // const title = data.itemInfo.title;
+      // const newprice = data.itemInfo.lowNowPrice;
+      // const oldprice = data.itemInfo.oldPrice;
+      // const columns = data.columns;
+      // const name = data.shopInfo.name;
+      // const shopLogo = data.shopInfo.shopLogo;
+      // const services = data.shopInfo.services;
+      // const cSells = data.shopInfo.cSells;
+      // const cGoods = data.shopInfo.cGoods;
+      // const score = data.shopInfo.score;
+      // const desc = data.detailInfo.desc;
+      // const detaiimg = data.detailInfo.detailImage
       this.setData({
-        topImg,title,newprice,oldprice,columns,name,shopLogo,services,cSells,cGoods,score,
-        desc,detaiimg,
+        // topImg,title,newprice,oldprice,columns,name,shopLogo,services,cSells,cGoods,score,
+        // desc,detaiimg,
+        Goods
       })
     })
   },
@@ -71,15 +75,57 @@ Page({
       // console.log(res)
     })
   },
-  // 2.0点击加入购物车把数据放到购物车中
-  
+  // 2.0点击加入购物车把数据放到购物车中 
   cartClick() {
-    // 2.1获取缓存中购物车的数组
-    // console.log(this.data.iid)
-    let cart =wx.getStorageSync('cart') || [];
-    // 2.2判断商品对象是否存在数组里
-    let index = this.data.iid
-    console.log(index)
+    // 2.1获取要存放到购物车的数据
+    // console.log(this.data.iid,this.data.title,this.data.newprice)
+    // var {iid,title,newprice,num} = this.data
+    // var obj = {iid,title,newprice,num}
+    // 2.2存放到缓存中
+    // wx.setStorage({
+    //   key: "cart",
+    //   data: JSON.stringify(obj),
+    //   success() {
+    //     wx.showToast({
+    //       title: '加入成功',
+    //     })
+    //   }
+    // })
+
+
+    // const pre_item = wx.getStorageSync('cart');
+    // const temp = {
+    //   "num": wx.getStorageSync('num'),
+    //   "iid": wx.getStorageSync('iid'),
+    //   "title": wx.getStorageSync('title'),
+    //   "newprice": wx.getStorageSync('newprice')
+    // }
+    // wx.setStorageSync('cart', [temp, ...pre_item])
+    // wx.showToast({
+    //   title: '加入购物车',
+    //   icon: 'success',
+    // })
+    
+    // 2.1获取缓存中的购物车数组
+    let cart = wx.getStorageSync('cart') || [];
+    // 2.2判断商品对象是否存在于购物车数组中
+    var goods_id = this.data.iid
+    console.log(goods_id,this.GoodsInfo.data.iid)
+    let index = cart.findIndex(v=>v.goods_id === this.GoodsInfo.data.iid)
+    if (index === -1) {
+      // 2.3数据不存在
+      this.GoodsInfo.num = 1;
+      cart.push(this.GoodsInfo);
+    }else{
+      // 2.4数据存在
+      cart[index].num++;
+    }
+    // 2.5把购物车从新添加回缓存中
+    wx.setStorageSync('cart', cart);
+    // 2.6弹窗提示
+    wx.showToast({
+      title: '加入成功',
+    })
   },
 
   /**
